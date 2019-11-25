9e1f4a2939d82071b4cc70d00b0dd94107f68d2b
Kristian Hellang <kristian@hellang.com>
2019-11-25 10:31:05 +0100

    Remove separate controller and model and move code into Startup

--- 3.distributed-calculator/csharp/Startup.cs
+++ 3.distributed-calculator/csharp/Startup.cs
@@ -3,45 +3,47 @@
 // Licensed under the MIT License.
 // ------------------------------------------------------------
 
+using System.Globalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
+using static System.Text.Json.JsonSerializer;
 
 namespace Subtract
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
+        public void Configure(IApplicationBuilder app)
         {
-            services.AddMvc();
-        }
-
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapPost("/subtract", async ctx =>
+                {
+                    var operands = await DeserializeAsync<Operands>(ctx.Request.Body);
+
+                    var result = decimal.Parse(operands.OperandOne) - decimal.Parse(operands.OperandTwo);
+
+                    ctx.Response.ContentType = "application/json";
+
+                    await ctx.Response.WriteAsync(result.ToString(CultureInfo.InvariantCulture));
+                });
             });
         }
 
         public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(x =>
-                    x.UseStartup<Startup>());
+                .ConfigureWebHostDefaults(x => x.UseStartup<Startup>())
+                .Build()
+                .Run();
+    }
+
+    public class Operands
+    {
+        public string OperandOne { get; set; }
+
+        public string OperandTwo { get; set; }
     }
 }
